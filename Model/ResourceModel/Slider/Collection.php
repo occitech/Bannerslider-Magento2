4f449b3c96dc7ece24ba4ecb18531dae61108702
Nicolas Pernot <nicolaspernot@yahoo.fr>
2023-08-08 23:20:35 +0200

    Correct some constructor types stuck on Magento 1 class naming

--- Model/ResourceModel/Slider/Collection.php
+++ Model/ResourceModel/Slider/Collection.php
@@ -71,23 +71,24 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
     }
 
     /**
-     * @param \Magento\Framework\Data\Collection\EntityFactoryInterface    $entityFactory
-     * @param \Psr\Log\LoggerInterface                                     $logger
+     * @param \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory
+     * @param \Psr\Log\LoggerInterface $logger
      * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
-     * @param \Magento\Framework\Event\ManagerInterface                    $eventManager
-     * @param \Zend_Db_Adapter_Abstract                                    $connection
-     * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb              $resource
+     * @param \Magento\Framework\Event\ManagerInterface $eventManager
+     * @param \Magento\Framework\DB\Adapter\AdapterInterface $connection
+     * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource
      */
     public function __construct(
-        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
-        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\EntityFactoryInterface    $entityFactory,
+        \Psr\Log\LoggerInterface                                     $logger,
         \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
-        \Magento\Framework\Event\ManagerInterface $eventManager,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Framework\Stdlib\DateTime\Timezone $stdTimezone,
-        $connection = null,
-        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
-    ) {
+        \Magento\Framework\Event\ManagerInterface                    $eventManager,
+        \Magento\Store\Model\StoreManagerInterface                   $storeManager,
+        \Magento\Framework\Stdlib\DateTime\Timezone                  $stdTimezone,
+                                                                     $connection = null,
+        \Magento\Framework\Model\ResourceModel\Db\AbstractDb         $resource = null
+    )
+    {
         parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $connection, $resource);
         $this->_storeManager = $storeManager;
         $this->_stdTimezone = $stdTimezone;
